af7bdc155f6af959aa965e341d2d6d47b3b753f4
darkhankz <upstudynow@gmail.com>
2023-02-17 11:28:13 +0600

    lesson-2

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "f53800b71df04d3499706eb0bc2c3341"
+    }
+}
