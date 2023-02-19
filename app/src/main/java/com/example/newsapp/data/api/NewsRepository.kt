4c9fb1aed0902ff5ca8bac22cdec57d0476b7c01
darkhankz <upstudynow@gmail.com>
2023-02-19 09:49:30 +0600

    lesson-5

--- app/src/main/java/com/example/newsapp/data/api/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/data/api/NewsRepository.kt
@@ -3,6 +3,9 @@ package com.example.newsapp.data.api
 import javax.inject.Inject
 
 class NewsRepository @Inject constructor(private val newsService: NewsService) {
-    suspend fun getNews(countryCode: String, pageNamber: Int) =
-        newsService.getHeadlines(countryCode = countryCode, page = pageNamber)
+    suspend fun getNews(countryCode: String, pageNumber: Int) =
+        newsService.getHeadlines(countryCode = countryCode, page = pageNumber)
+
+    suspend fun getSearchNews(query: String, pageNumber: Int) = newsService.getEverything(query = query, page = pageNumber)
+
 }
--- app/src/main/java/com/example/newsapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/main/MainViewModel.kt
@@ -22,7 +22,7 @@ class MainViewModel @Inject constructor(private val repository: NewsRepository):
     private fun getNews(countryCode: String){
         viewModelScope.launch {
             newsLiveData.postValue(Resource.Loading())
-            val response = repository.getNews(countryCode = countryCode, pageNamber = newsPage)
+            val response = repository.getNews(countryCode = countryCode, pageNumber = newsPage)
             if (response.isSuccessful){
                 response.body().let {res ->
                     newsLiveData.postValue(Resource.Success(res))
